32ca02073d4f703f1bf76340d5d59a65409f6535
yangzhi <3327595753@qq.com>
2022-07-14 17:44:49 +0800

    在克隆下来的项目中进行第一次自主开发

---
+++ code.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+typedef long long LL;
+
+int main()
+{
+	int git = 520;
+	int hub = 1314;
+	cout << git  + hub <<endl;
+	
+	cout << "GitHub YYDS " << endl; 
+	return 0;
+}
